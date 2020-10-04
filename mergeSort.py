f5b0ef9d336479aff0915301142be2cc33521ce4
anlu408 <71849385+anlu408@users.noreply.github.com>
2020-10-03 22:43:40 -0700

    Create mergeSort.py
    Added mergeSort

---
+++ mergeSort.py
@@ -0,0 +1,37 @@
+a = [11,21,33,44]
+b = [12,52,64]
+c = a + b
+c=[11,21,33,44,52,12,64]
+
+#C at this point: [11, 21, 33, 12, 52, 64]
+#Expected output: [11, 12, 21, 33, 52, 64]
+
+middle = int(len(c) / 2)
+
+left = c[:middle]
+right = c[middle:]
+
+d = int(len(left))
+e = int(len(right))
+
+i = j = k = 0
+
+while((i < d) and (j < e)):
+    if(left[i] < right[j]):
+        c[k] = left[i]
+        i += 1
+    else:
+        c[k] = right[j]
+        j += 1
+    k += 1
+
+while (i<d):
+    c[k] = left[i]
+    i += 1
+    k += 1
+while (j < e):
+    c[k] = right[j]
+    j  += 1
+    k += 1
+
+print(c)

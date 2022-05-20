32bd20a654fc10e7924876ccfc4ba2a2b96960d6
anlu408 <71849385+anlu408@users.noreply.github.com>
2022-05-20 04:52:24 -0700

    Update graph.py
    completed breadth first search

--- Data Structures/Graphs/graph.py
+++ Data Structures/Graphs/graph.py
@@ -1,3 +1,14 @@
+'''
+Notes section
+BFS- Breadth First Search
+An algorithm for traversing Graphs starting at an arbitrary node and exploring
+neighbor nodes at the current level before moving to the next level.
+
+Use a queue to enqueue a starting vertex, while the queue isn't empty,
+dequeue. If the node the algorithm is currently on is unvisited, mark it
+as visited, and enqueue the adjacent nodes.
+'''
+
 class graph:
     def __init__(self, dictionary = None):
         if dictionary is None:
@@ -6,3 +17,15 @@ class graph:
 
     def add_edge(self,vertex,edge):
         self.dictionary[vertex].append(edge)
+
+    def bfs (self, vertex):
+        visited = [vertex]
+        queue = [vertex]
+
+        while queue:
+            dequeue = queue.pop(0)
+            print(dequeue)
+            for adjacent_vertex in self.gdict[dequeue]:
+                if adjacent_vertex not in visited:
+                    visited.append(adjacent_vertex)
+                    queue.append(adjacent_vertex)
